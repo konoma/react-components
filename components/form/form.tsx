4af81d71a00f3759219eac4df57fb47aa2e45e7e
Felix Grafschmidt <felix.grafschmidt@konoma.ch>
2025-12-03 10:35:01 +0100

    form onValidation has information on trigger type

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -29,37 +29,40 @@ export default function Form<DataType>({
   data: DataType;
   dataTestId?: string;
   validators: Record<keyof DataType, ((value: string | number | boolean | null) => string)[]>;
-  onValidation?: (errors: Record<keyof DataType, string[]>) => void;
+  onValidation?: (errors: Record<keyof DataType, string[]>, triggeredBySubmit?: boolean) => void;
   onSubmit: () => Promise<void>;
 }) {
   const [errors, setErrors] = useState<Record<string, string[]>>({} as Record<string, string[]>);
   const [submitAttempted, setSubmitAttempted] = useState(false);
 
-  const validate = useCallback(async () => {
-    let invalid = false;
-    const newErrors: Record<keyof DataType, string[]> = {} as Record<keyof DataType, string[]>;
+  const validate = useCallback(
+    async (triggeredBySubmit?: boolean) => {
+      let invalid = false;
+      const newErrors: Record<keyof DataType, string[]> = {} as Record<keyof DataType, string[]>;
 
-    Object.entries(data as Record<string, (string | number | boolean) | null>).forEach(([name, value]) => {
-      if (!validators[name as keyof DataType]) {
-        return;
+      Object.entries(data as Record<string, (string | number | boolean) | null>).forEach(([name, value]) => {
+        if (!validators[name as keyof DataType]) {
+          return;
+        }
+        newErrors[name as keyof DataType] = validators[name as keyof DataType]
+          .map((validator) => {
+            const validation = validator(value);
+            if (validation) {
+              invalid = true;
+            }
+            return validation;
+          })
+          .filter((v) => v);
+      });
+      setErrors(newErrors);
+      onValidation(newErrors, triggeredBySubmit);
+      if (process.env.NODE_ENV === 'development') {
+        console.info('Form errors', newErrors);
       }
-      newErrors[name as keyof DataType] = validators[name as keyof DataType]
-        .map((validator) => {
-          const validation = validator(value);
-          if (validation) {
-            invalid = true;
-          }
-          return validation;
-        })
-        .filter((v) => v);
-    });
-    setErrors(newErrors);
-    onValidation(newErrors);
-    if (process.env.NODE_ENV === 'development') {
-      console.info('Form errors', newErrors);
-    }
-    return invalid;
-  }, [data]);
+      return invalid;
+    },
+    [data]
+  );
 
   useEffect(() => {
     if (submitAttempted) {
@@ -69,7 +72,7 @@ export default function Form<DataType>({
 
   async function submit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const invalid = await validate();
+    const invalid = await validate(true);
 
     if (invalid) {
       setSubmitAttempted(true);
