935ae5706d63a50e9532800a5cd8d0f3a20a8e61
Felix Grafschmidt <felix.grafschmidt@konoma.ch>
2026-01-21 09:48:54 +0100

    skip scroll animation

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -239,7 +239,7 @@ export default function Table<DataType extends { dragRef?: React.RefObject<HTMLD
             currentColumnsRight.findIndex((col) => col.filterKey === triggeredFilter);
     const columnElement = header.current?.children.item(columnIndex) as HTMLDivElement | null;
     if (columnElement) {
-      columnElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+      columnElement.scrollIntoView({ behavior: 'instant', block: 'nearest', inline: 'center' });
     }
   }, []);
 
